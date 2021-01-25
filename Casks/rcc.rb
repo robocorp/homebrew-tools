91d860013c583e00506d0cb29c09184178ce23d3
Tuomas <tuomas@robocorp.com>
2021-01-25 15:07:48 +0000

    rcc updated to 8.0.12

--- Casks/rcc.rb
+++ Casks/rcc.rb
@@ -1,9 +1,9 @@
 cask "rcc" do
-  version "7.0.7"
-  sha256 "719e8d291a7c1d7984c41213aeb8450fcb2f8162c3efa99edcf8327735ace1ec"
+  version "8.0.12"
+  sha256 "89884520fdf1deeaad71608f1ea171979528b1252547dc5cab5a9c3a776a64d1"
 
   # robocorp.com was verified as official when first introduced to the cask
-  url "https://downloads.code.robocorp.com/rcc/v#{version}/macos64/rcc"
+  url "https://downloads.robocorp.com/rcc/releases/v#{version}/macos64/rcc"
   name "rcc"
   desc "Tooling for managing and distributing self-contained automation robots"
   homepage "https://github.com/robocorp/rcc"

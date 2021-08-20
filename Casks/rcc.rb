e25a510d6c5c1166e5085252190da455c2687290
Tuomas <tuomas@robocorp.com>
2021-08-20 09:17:37 +0000

    rcc updated to 10.7.1

--- Casks/rcc.rb
+++ Casks/rcc.rb
@@ -1,6 +1,6 @@
 cask "rcc" do
-  version "9.16.0"
-  sha256 "f9f1b2d933664946286da19b22a60b41c1cd56ce988efe2048da9b67fb69aa67"
+  version "10.7.1"
+  sha256 "9fecea3c3395615f2dfdb36d228c370f0fe9c33e65b637d80d91cd20464c14b1"
 
   # robocorp.com was verified as official when first introduced to the cask
   url "https://downloads.robocorp.com/rcc/releases/v#{version}/macos64/rcc"

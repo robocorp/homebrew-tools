bdeaeed5160644b8b23deef6584c1a3e980c3bfe
Kerkko <kerkko@robocorp.com>
2023-05-12 08:41:53 +0000

    robo updated to 0.2.10

--- Casks/robo.rb
+++ Casks/robo.rb
@@ -3,7 +3,7 @@ cask "robo" do
   sha256 "2a4ac683ac079bec5896d3fef0b4bd7a361494699e723217ccd75f1e46a3a97f"
 
   # robocorp.com was verified as official when first introduced to the cask
-  url "https://downloads.robocorp.com/robo/releases/v#{version}/macos64/robo"
+  url "https://downloads.robocorp.com/robo/releases/#{version}/macos64/robo"
   name "robo"
   desc "Command line tool for creating and maintaining Python automation projects."
   homepage "https://github.com/robocorp/robo"

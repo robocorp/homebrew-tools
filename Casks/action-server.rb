935526adb5154cb6ffb1fa3512ec79aad4792b0d
Kari <kari@robocorp.com>
2024-01-12 00:43:02 +0000

    action-server updated to 0.0.12

--- Casks/action-server.rb
+++ Casks/action-server.rb
@@ -1,6 +1,6 @@
 cask "action-server" do
-  version "0.0.11"
-  sha256 "60e1482689f85879b584ecd958a9d2599881e551f53ca1ba2622d066ba1d12cf"
+  version "0.0.12"
+  sha256 "f7fd82bceece2ed07c9bac9ef8fc48b4735a020fef82fe1205065e4c7e16461c"
 
   # robocorp.com was verified as official when first introduced to the cask
   url "https://downloads.robocorp.com/action-server/releases/#{version}/macos64/action-server"
